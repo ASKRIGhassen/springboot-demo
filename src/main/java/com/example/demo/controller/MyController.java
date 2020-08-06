e98263431f448a220329657868a49db893120a20
ASKRIGhassen <gh.askri@gmail.com>
2020-08-06 11:42:30 +0200

    first commit

---
+++ src/main/java/com/example/demo/controller/MyController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class MyController {
+
+    @GetMapping("/indexes")
+    public Map<String,Object> getIndex(){
+        Map<String,Object> result = new HashMap<>();
+        result.put("hello","world");
+        return result;
+    }
+}
